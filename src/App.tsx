857e2b4f40d844a0c9128d5f3137b02588bb672f
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-12-03 20:01:01 +0400

    remove-react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import React, { FC } from "react";
 import "./App.css";
 import Game from "./components/Game";
 import Header from "./components/Header";
-import { BrowserRouter as Router } from "react-router-dom";
-// import { Route } from "react-router-dom";
 
 const App: FC = () => (
   <div className="App">
-    {/* <Header />
-    <Game /> */}
     <Header />
-    <Router>{/* <Route exact path="/" component={Game} /> */}</Router>
+    <Game />
   </div>
 );
 
