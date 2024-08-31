69c46c94bcea2ce2a85a172de691c17740098507
mrrustemka <mubarakshin1996@gmail.com>
2024-08-31 16:02:24 +0400

    add-play-off-rank

--- src/components/play-off/PlayOff.tsx
+++ src/components/play-off/PlayOff.tsx
@@ -1,6 +1,6 @@
-﻿import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import Round from './Round';
+﻿import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Round from "./Round";
 import { v4 as uuidv4 } from "uuid";
 import { teams as mock } from "../../data/teams";
 
@@ -12,6 +12,10 @@ function PlayOff() {
 
   const location = useLocation();
   // const { teams } = location.state || {};
+
+  for (let i = 0; i < teams.length; i++) {
+    teams[i].play_off_rank = i + 1;
+  }
 
   if (isRound) {
     let newRound: Array<any> = [];
@@ -25,12 +29,18 @@ function PlayOff() {
     setIsRound(!isRound);
   }
 
+  console.log(teams);
+
   return (
     <div>
       {tree.map((value, index) => (
         <Round
           key={uuidv4()}
-          abv={index === Math.log2(teams.length) - 1 ? 'Final' : 'Round ' + (index + 1)}
+          abv={
+            index === Math.log2(teams.length) - 1
+              ? "Final"
+              : "Round " + (index + 1)
+          }
           pairs={value}
         />
       ))}
