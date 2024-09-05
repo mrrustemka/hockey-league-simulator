d0fb20209dbbb46ba61d8c8df71504236ef00ec8
mrrustemka <mubarakshin1996@gmail.com>
2024-09-05 13:20:24 +0400

    fix-sheet-class-names-according-to-bem

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -15,11 +15,13 @@ function Sheet(props: { teamsData: Teams[] }) {
           rank++;
           return (
             <div>
-              <p>{rank > props.teamsData.length ? (rank = 0) : rank}</p>
+              <p className="table__column-rank">
+                {rank > props.teamsData.length ? (rank = 0) : rank}
+              </p>
             </div>
           );
         }}
-        className="table-column-rank"
+        className="table__column table__column--rank"
       />
       <Table.Column
         title="Team"
@@ -30,18 +32,18 @@ function Sheet(props: { teamsData: Teams[] }) {
             (item: { name: any }) => item.name === dataIndex
           )?.logo;
           return (
-            <div>
+            <div className="table__team">
               <img
                 src={logo}
                 alt={dataIndex + " Logo"}
                 width={"16px"}
-                className="table-team-logo"
+                className="table__team-logo"
               />
-              <p className="table-column-name">{" " + dataIndex}</p>
+              <p className="table__column-name">{" " + dataIndex}</p>
             </div>
           );
         }}
-        className="column-name"
+        className="table__column table__column--name"
       />
       <Table.Column
         title="Rating"
@@ -50,13 +52,13 @@ function Sheet(props: { teamsData: Teams[] }) {
         sorter={(a: { rating: number }, b: { rating: number }) =>
           a.rating - b.rating
         }
-        className="table-column-stats"
+        className="table__column table__column--rating"
       />
       <Table.Column
         title="City"
         dataIndex="city"
         key="city"
-        className="table-column-stats"
+        className="table__column table__column--city"
       />
       <Table.Column
         title="GP"
@@ -69,14 +71,14 @@ function Sheet(props: { teamsData: Teams[] }) {
           multiple: 4
         }}
         render={(gameCounter) => {
-          let className = "table-column-stats";
+          let className = "table__column-games";
 
           if (gameCounter > [...props.teamsData].length * 0.75) {
-            className = "table-column-stats-high";
+            className = "table__column-games--high";
           } else if (gameCounter > [...props.teamsData].length * 0.5) {
-            className = "table-column-stats-medium";
+            className = "table__column-games--medium";
           } else if (gameCounter > [...props.teamsData].length * 0.25) {
-            className = "table-column-stats-low";
+            className = "table__column-games--low";
           }
 
           return (
@@ -85,7 +87,7 @@ function Sheet(props: { teamsData: Teams[] }) {
             </div>
           );
         }}
-        className="table-column-stats"
+        className="table__column table__column--games"
       />
       <Table.Column
         title="W"
@@ -97,13 +99,13 @@ function Sheet(props: { teamsData: Teams[] }) {
             a.wins - b.wins,
           multiple: 3
         }}
-        className="table-column-stats"
+        className="table__column table__column--wins"
       />
       <Table.Column
         title="L"
         dataIndex="loses"
         key="loses"
-        className="table-column-stats"
+        className="table__column table__column--loses"
       />
       <Table.Column
         title="OT"
@@ -115,7 +117,7 @@ function Sheet(props: { teamsData: Teams[] }) {
             b.loses_ot - a.loses_ot,
           multiple: 2
         }}
-        className="table-column-stats"
+        className="table__column table__column--loses-ot"
       />
       <Table.Column
         title="PTS"
@@ -127,7 +129,7 @@ function Sheet(props: { teamsData: Teams[] }) {
             a.points - b.points,
           multiple: 5
         }}
-        className="table-column-points table-table-column-stats"
+        className="table__column table__column--points"
       />
       <Table.Column
         title="DIFF"
@@ -145,10 +147,10 @@ function Sheet(props: { teamsData: Teams[] }) {
               <p
                 className={
                   dataIndex > 0
-                    ? "table-column-diff-positive"
+                    ? "table__column-diff--positive"
                     : dataIndex === 0
                     ? ""
-                    : "table-column-diff-negative"
+                    : "table__column-diff--negative"
                 }
               >
                 {dataIndex > 0 ? "+" + dataIndex : dataIndex}
@@ -156,7 +158,7 @@ function Sheet(props: { teamsData: Teams[] }) {
             </div>
           );
         }}
-        className="table-column-stats"
+        className="table__column table__column--diff"
       />
     </Table>
   );
