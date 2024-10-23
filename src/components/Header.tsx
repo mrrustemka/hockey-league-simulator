a445e13070b178c1750a932669b31edf75cf8c89
mrrustemka <mubarakshin1996@gmail.com>
2024-10-23 19:04:37 +0400

    fix-any-type-in-header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,28 @@ import { Typography } from "antd";
 import { champs } from "../Data/champs";
 
 const { Title } = Typography;
-function Header({ id }: any) {
+function Header(props: { id: string }) {
   const whiteList: string[] = ["5"];
   return (
     <header className="header">
-      {id === "" ? (
+      {props.id === "" ? (
         <></>
       ) : (
         <img
           className="header__image"
-          src={champs.find((champ) => champ.id === id)?.image}
-          alt={champs.find((champ) => champ.id === id)?.name + " Logo"}
+          src={champs.find((champ) => champ.id === props.id)?.image}
+          alt={champs.find((champ) => champ.id === props.id)?.name + " Logo"}
           style={{
-            backgroundColor: whiteList.includes(id) ? "#ffffff" : "transparent"
+            backgroundColor: whiteList.includes(props.id)
+              ? "#ffffff"
+              : "transparent"
           }}
         />
       )}
       <Title className="header__title">
-        {id === ""
+        {props.id === ""
           ? "Select a Championship"
-          : champs.find((champ) => champ.id === id)?.name}
+          : champs.find((champ) => champ.id === props.id)?.name}
       </Title>
     </header>
   );
