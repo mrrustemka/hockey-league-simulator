9ff87c9659dd5ab483ff145bd4aa7d583995865f
mrrustemka <mubarakshin1996@gmail.com>
2024-10-29 15:35:43 +0400

    implement-league-component

---
+++ src/components/League.tsx
@@ -0,0 +1,115 @@
+import { Card, Typography, Input } from "antd";
+import { v4 as uuidv4 } from "uuid";
+import { schedule } from "../Data/schedule";
+import { Link } from "react-router-dom";
+import { League as TLeague } from "../Data/types";
+
+const { Title } = Typography;
+
+function League(props: {
+  league: TLeague;
+  updateTeamsNum: (id: string, value: number) => void;
+}) {
+  return (
+    <div>
+      <Card
+        key={uuidv4()}
+        className="start__card"
+        cover={
+          <img
+            alt="example"
+            src={props.league.image}
+            className="start__card-cover"
+          />
+        }
+      >
+        <Title level={5} className="start__card-title">
+          {props.league.description}
+        </Title>
+        <div className="start__teams">
+          {props.league.teams.map(
+            (team: {
+              id: number;
+              logo: string | undefined;
+              name: string | undefined;
+            }) => (
+              <div
+                key={uuidv4()}
+                className={`start__team ${
+                  team.id <= props.league.teamsCount
+                    ? "start__team--active"
+                    : ""
+                }`}
+              >
+                <img
+                  src={team.logo}
+                  alt={team.name}
+                  className="start__team-logo"
+                />
+              </div>
+            )
+          )}
+        </div>
+        <div className="start__actions">
+          <Title level={5} className="start__actions-title--play-off-count">
+            {props.league.teamsCount > 16
+              ? 16
+              : props.league.teamsCount > 8
+              ? 8
+              : props.league.teamsCount > 4
+              ? 4
+              : 2}{" "}
+            teams will advance to the Play-Off
+          </Title>
+          <Title level={5} className="start__actions-title--selection">
+            Select the number of teams
+          </Title>
+          <Input
+            className={`start__input`}
+            type="number"
+            defaultValue={props.league.teamsCount}
+            value={props.league.teamsCount}
+            max={props.league.teams.length}
+            min="2"
+            onChange={(e) =>
+              props.updateTeamsNum(props.league.id, parseInt(e.target.value))
+            }
+          />
+          <Title
+            level={5}
+            className={`start__actions-error ${
+              props.league.teamsCount < 2 ||
+              props.league.teamsCount > props.league.teams.length
+                ? "start__actions-error--active"
+                : "start__actions-error--inactive"
+            }`}
+          >
+            Please enter a valid number of teams. From 2 to{" "}
+            {props.league.teams.length}
+          </Title>
+          <Link
+            className={`start__link ${
+              props.league.teamsCount < 2 ||
+              props.league.teamsCount > props.league.teams.length
+                ? "start__link--inactive"
+                : "start__link--active pulse"
+            }`}
+            to="season"
+            state={{
+              teams: props.league.teams.slice(0, props.league.teamsCount),
+              name: props.league.name,
+              id: props.league.id
+            }}
+            onClick={() =>
+              schedule(props.league.teams.slice(0, props.league.teamsCount))
+            }
+          >
+            Start
+          </Link>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default League;
--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,16 +1,11 @@
-import { Card, Input, Typography } from "antd";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { League } from "../Data/types";
-import { v4 as uuidv4 } from "uuid";
-import { schedule } from "../Data/schedule";
+import { League as TLeague } from "../Data/types";
 import { champs } from "../Data/champs";
 import Header from "./Header";
-
-const { Title } = Typography;
+import League from "./League";
 
 function Start() {
-  const [champTypes, setChampTypes] = useState<League[]>(champs);
+  const [champTypes, setChampTypes] = useState<TLeague[]>(champs);
 
   function updateTeamsNum(id: string, value: number): void {
     const newChampTypes = champTypes.map((champ) =>
@@ -24,90 +19,11 @@ function Start() {
       <Header id="" />
       <div className="start">
         {champTypes.map((champ) => (
-          <Card
-            key={uuidv4()}
-            className="start__card"
-            cover={
-              <img
-                alt="example"
-                src={champ.image}
-                className="start__card-cover"
-              />
-            }
-          >
-            <Title level={5} className="start__card-title">
-              {champ.description}
-            </Title>
-            <div className="start__teams">
-              {champ.teams.map((team) => (
-                <div
-                  key={uuidv4()}
-                  className={`start__team ${
-                    team.id <= champ.teamsCount ? "start__team--active" : ""
-                  }`}
-                >
-                  <img
-                    src={team.logo}
-                    alt={team.name}
-                    className="start__team-logo"
-                  />
-                </div>
-              ))}
-            </div>
-            <div className="start__actions">
-              <Title level={5} className="start__actions-title--play-off-count">
-                {champ.teamsCount > 16
-                  ? 16
-                  : champ.teamsCount > 8
-                  ? 8
-                  : champ.teamsCount > 4
-                  ? 4
-                  : 2}{" "}
-                teams will advance to the Play-Off
-              </Title>
-              <Title level={5} className="start__actions-title--selection">
-                Select the number of teams
-              </Title>
-              <Input
-                className={`start__input`}
-                type="number"
-                defaultValue={champ.teamsCount}
-                value={champ.teamsCount}
-                max={champ.teams.length}
-                min="2"
-                onChange={(e) =>
-                  updateTeamsNum(champ.id, parseInt(e.target.value))
-                }
-              />
-              <Title
-                level={5}
-                className={`start__actions-error ${
-                  champ.teamsCount < 2 || champ.teamsCount > champ.teams.length
-                    ? "start__actions-error--active"
-                    : "start__actions-error--inactive"
-                }`}
-              >
-                Please enter a valid number of teams. From 2 to{" "}
-                {champ.teams.length}
-              </Title>
-              <Link
-                className={`start__link ${
-                  champ.teamsCount < 2 || champ.teamsCount > champ.teams.length
-                    ? "start__link--inactive"
-                    : "start__link--active pulse"
-                }`}
-                to="season"
-                state={{
-                  teams: champ.teams.slice(0, champ.teamsCount),
-                  name: champ.name,
-                  id: champ.id
-                }}
-                onClick={() => schedule(champ.teams.slice(0, champ.teamsCount))}
-              >
-                Start
-              </Link>
-            </div>
-          </Card>
+          <League
+            key={champ.id}
+            league={champ}
+            updateTeamsNum={updateTeamsNum}
+          />
         ))}
       </div>
     </>
