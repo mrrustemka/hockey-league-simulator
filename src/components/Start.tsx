de8b1dbf2f9f9cee770bd7254be0209cef530e3d
mrrustemka <mubarakshin1996@gmail.com>
2024-10-30 08:06:48 +0400

    fix-styles

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -3,15 +3,6 @@ import Header from "./Header";
 import League from "./League";
 
 function Start() {
-  // const [champTypes, setChampTypes] = useState<TLeague[]>(champs);
-
-  // function updateTeamsNum(id: string, value: number): void {
-  //   const newChampTypes = champTypes.map((champ) =>
-  //     champ.id === id ? { ...champ, teamsCount: value } : { ...champ }
-  //   );
-  //   setChampTypes(newChampTypes);
-  // }
-
   return (
     <>
       <Header id="" />
