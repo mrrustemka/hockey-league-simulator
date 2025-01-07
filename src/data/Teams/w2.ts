c0547e0c0a5a63d84b9d126b6cc0de9cecaa672a
mrrustemka <mubarakshin1996@gmail.com>
2025-01-07 19:59:07 +0500

    implement-w2-gallery

--- src/data/Teams/w2.ts
+++ src/data/Teams/w2.ts
@@ -1,16 +1,43 @@
 import { Teams } from "../types";
-import gbr from "../../Images/w2_gbr.png";
-import pol from "../../Images/w2_pol.png";
-import ita from "../../Images/w2_ita.png";
-import skr from "../../Images/w2_skr.png";
-import rom from "../../Images/w2_rom.png";
-import jap from "../../Images/w2_jap.png";
 import chi from "../../Images/w2_chi.png";
-import est from "../../Images/w2_est.png";
-import lit from "../../Images/w2_lit.png";
+import chi_1 from "../../Images/Gallery/W2/Chi/chi_1.jpg";
+import chi_2 from "../../Images/Gallery/W2/Chi/chi_2.jpg";
 import cro from "../../Images/w2_cro.png";
+import cro_1 from "../../Images/Gallery/W2/Cro/cro_1.jpg";
+import cro_2 from "../../Images/Gallery/W2/Cro/cro_2.jpg";
+import est from "../../Images/w2_est.png";
+import est_1 from "../../Images/Gallery/W2/Est/est_1.jpg";
+import est_2 from "../../Images/Gallery/W2/Est/est_2.jpg";
+import est_3 from "../../Images/Gallery/W2/Est/est_3.jpg";
+import gbr from "../../Images/w2_gbr.png";
+import gbr_1 from "../../Images/Gallery/W2/Gbr/gbr_1.jpg";
+import gbr_2 from "../../Images/Gallery/W2/Gbr/gbr_2.jpg";
+import gbr_3 from "../../Images/Gallery/W2/Gbr/gbr_3.jpg";
+import ita from "../../Images/w2_ita.png";
+import ita_1 from "../../Images/Gallery/W2/Ita/ita_1.jpg";
+import ita_2 from "../../Images/Gallery/W2/Ita/ita_2.jpg";
+import ita_3 from "../../Images/Gallery/W2/Ita/ita_3.jpg";
+import jap from "../../Images/w2_jap.png";
+import jap_1 from "../../Images/Gallery/W2/Jap/jap_1.jpg";
+import jap_2 from "../../Images/Gallery/W2/Jap/jap_2.jpg";
+import lit from "../../Images/w2_lit.png";
+import lit_1 from "../../Images/Gallery/W2/Lit/lit_1.jpg";
+import lit_2 from "../../Images/Gallery/W2/Lit/lit_2.jpg";
+import pol from "../../Images/w2_pol.png";
+import pol_1 from "../../Images/Gallery/W2/Pol/pol_1.jpg";
+import pol_2 from "../../Images/Gallery/W2/Pol/pol_2.jpg";
+import rom from "../../Images/w2_rom.png";
+import rom_1 from "../../Images/Gallery/W2/Rom/rom_1.jpg";
+import rom_2 from "../../Images/Gallery/W2/Rom/rom_2.jpg";
+import skr from "../../Images/w2_skr.png";
+import skr_1 from "../../Images/Gallery/W2/Skr/skr_1.jpg";
+import skr_2 from "../../Images/Gallery/W2/Skr/skr_2.jpg";
 import spa from "../../Images/w2_spa.png";
+import spa_1 from "../../Images/Gallery/W2/Spa/spa_1.jpg";
+import spa_2 from "../../Images/Gallery/W2/Spa/spa_2.jpg";
 import ukr from "../../Images/w2_ukr.png";
+import ukr_1 from "../../Images/Gallery/W2/Ukr/ukr_1.jpg";
+import ukr_2 from "../../Images/Gallery/W2/Ukr/ukr_2.jpg";
 
 export const w2: Teams[] = [
   {
@@ -36,7 +63,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [gbr_1, gbr_2, gbr_3]
   },
   {
     abbreviation: "POL",
@@ -61,7 +89,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [pol_1, pol_2]
   },
   {
     abbreviation: "ITA",
@@ -86,7 +115,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [ita_1, ita_2, ita_3]
   },
   {
     abbreviation: "SKR",
@@ -111,7 +141,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [skr_1, skr_2]
   },
   {
     abbreviation: "ROM",
@@ -136,7 +167,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [rom_1, rom_2]
   },
   {
     abbreviation: "JAP",
@@ -161,7 +193,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [jap_1, jap_2]
   },
   {
     abbreviation: "CHI",
@@ -186,7 +219,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [chi_1, chi_2]
   },
   {
     abbreviation: "EST",
@@ -211,7 +245,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [est_1, est_2, est_3]
   },
   {
     abbreviation: "LIT",
@@ -236,7 +271,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [lit_1, lit_2]
   },
   {
     abbreviation: "CRO",
@@ -261,7 +297,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [cro_1, cro_2]
   },
   {
     abbreviation: "SPA",
@@ -286,7 +323,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [spa_1, spa_2]
   },
   {
     abbreviation: "UKR",
@@ -311,7 +349,8 @@ export const w2: Teams[] = [
     status: "",
     curStatusLength: 0,
     chartData: [],
-    chartLabels: []
+    chartLabels: [],
+    photos: [ukr_1, ukr_2]
   }
   // {
   //   abbreviation: "NET",
