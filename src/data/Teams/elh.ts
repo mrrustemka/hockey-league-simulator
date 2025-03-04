f744f758f5f861b1a09d94a5edaa06f5f2462344
mrrustemka <mubarakshin1996@gmail.com>
2025-03-04 19:55:31 +0500

    implement-ELH-team-stadiums

--- src/data/Teams/elh.ts
+++ src/data/Teams/elh.ts
@@ -4,68 +4,82 @@ import ceb_1 from "../../Images/Gallery/Elh/Ceb/ceb_1.jpg";
 import ceb_2 from "../../Images/Gallery/Elh/Ceb/ceb_2.jpg";
 import ceb_3 from "../../Images/Gallery/Elh/Ceb/ceb_3.jpg";
 import ceb_4 from "../../Images/Gallery/Elh/Ceb/ceb_4.jpg";
+import ceb_5 from "../../Images/Gallery/Elh/Ceb/ceb_5.webp";
 import kla from "../../Images/elh_kla.png";
 import kla_1 from "../../Images/Gallery/Elh/Kla/kla_1.jpg";
 import kla_2 from "../../Images/Gallery/Elh/Kla/kla_2.jpg";
 import kla_3 from "../../Images/Gallery/Elh/Kla/kla_3.jpg";
+import kla_4 from "../../Images/Gallery/Elh/Kla/kla_4.webp";
 import kom from "../../Images/elh_kom.png";
 import kom_1 from "../../Images/Gallery/Elh/Kom/kom_1.jpg";
 import kom_2 from "../../Images/Gallery/Elh/Kom/kom_2.jpeg";
 import kom_3 from "../../Images/Gallery/Elh/Kom/kom_3.jpg";
+import kom_4 from "../../Images/Gallery/Elh/Kom/kom_4.webp";
 import kva from "../../Images/elh_kva.png";
 import kva_1 from "../../Images/Gallery/Elh/Kva/kva_1.jpg";
 import kva_2 from "../../Images/Gallery/Elh/Kva/kva_2.jpg";
 import kva_3 from "../../Images/Gallery/Elh/Kva/kva_3.jpg";
 import kva_4 from "../../Images/Gallery/Elh/Kva/kva_4.jpg";
-import kva_5 from "../../Images/Gallery/Elh/Kva/kva_4.jpg";
+import kva_5 from "../../Images/Gallery/Elh/Kva/kva_5.jpg";
+import kva_6 from "../../Images/Gallery/Elh/Kva/kva_6.webp";
 import lib from "../../Images/elh_lib.png";
 import lib_1 from "../../Images/Gallery/Elh/Lib/lib_1.jpg";
 import lib_2 from "../../Images/Gallery/Elh/Lib/lib_2.jpg";
 import lib_3 from "../../Images/Gallery/Elh/Lib/lib_3.jpg";
+import lib_4 from "../../Images/Gallery/Elh/Lib/lib_4.webp";
 import lit from "../../Images/elh_lit.png";
 import lit_1 from "../../Images/Gallery/Elh/Lit/lit_1.jpg";
 import lit_2 from "../../Images/Gallery/Elh/Lit/lit_2.jpg";
 import lit_3 from "../../Images/Gallery/Elh/Lit/lit_3.jpg";
 import lit_4 from "../../Images/Gallery/Elh/Lit/lit_4.jpg";
+import lit_5 from "../../Images/Gallery/Elh/Lit/lit_5.webp";
 import mbl from "../../Images/elh_mbl.png";
 import mbl_1 from "../../Images/Gallery/Elh/Mbl/mbl_1.jpg";
 import mbl_2 from "../../Images/Gallery/Elh/Mbl/mbl_2.jpg";
 import mbl_3 from "../../Images/Gallery/Elh/Mbl/mbl_3.jpg";
 import mbl_4 from "../../Images/Gallery/Elh/Mbl/mbl_4.jpg";
+import mbl_5 from "../../Images/Gallery/Elh/Mbl/mbl_5.webp";
 import mhk from "../../Images/elh_mhk.png";
 import mhk_1 from "../../Images/Gallery/Elh/Mhk/mhk_1.jpg";
 import mhk_2 from "../../Images/Gallery/Elh/Mhk/mhk_2.jpg";
 import mhk_3 from "../../Images/Gallery/Elh/Mhk/mhk_3.jpg";
 import mhk_4 from "../../Images/Gallery/Elh/Mhk/mhk_4.jpg";
+import mhk_5 from "../../Images/Gallery/Elh/Mhk/mhk_5.webp";
 import olo from "../../Images/elh_olo.png";
 import olo_1 from "../../Images/Gallery/Elh/Olo/olo_1.jpg";
 import olo_2 from "../../Images/Gallery/Elh/Olo/olo_2.jpg";
 import olo_3 from "../../Images/Gallery/Elh/Olo/olo_3.jpg";
+import olo_4 from "../../Images/Gallery/Elh/Olo/olo_4.webp";
 import pce from "../../Images/ehl_pce.png";
 import pce_1 from "../../Images/Gallery/Elh/Pce/pce_1.jpg";
 import pce_2 from "../../Images/Gallery/Elh/Pce/pce_2.jpg";
 import pce_3 from "../../Images/Gallery/Elh/Pce/pce_3.jpg";
 import pce_4 from "../../Images/Gallery/Elh/Pce/pce_4.jpg";
 import pce_5 from "../../Images/Gallery/Elh/Pce/pce_5.jpg";
+import pce_6 from "../../Images/Gallery/Elh/Pce/pce_6.webp";
 import plz from "../../Images/elh_plz.png";
 import plz_1 from "../../Images/Gallery/Elh/Plz/plz_1.jpg";
 import plz_2 from "../../Images/Gallery/Elh/Plz/plz_2.jpg";
 import plz_3 from "../../Images/Gallery/Elh/Plz/plz_3.jpg";
+import plz_4 from "../../Images/Gallery/Elh/Plz/plz_4.webp";
 import spa from "../../Images/ehl_spa.png";
 import spa_1 from "../../Images/Gallery/Elh/Spa/spa_1.jpg";
 import spa_2 from "../../Images/Gallery/Elh/Spa/spa_2.jpg";
 import spa_3 from "../../Images/Gallery/Elh/Spa/spa_3.jpg";
 import spa_4 from "../../Images/Gallery/Elh/Spa/spa_4.jpg";
+import spa_5 from "../../Images/Gallery/Elh/Spa/spa_5.webp";
 import tri from "../../Images/elh_tri.png";
 import tri_1 from "../../Images/Gallery/Elh/Tri/tri_1.jpg";
 import tri_2 from "../../Images/Gallery/Elh/Tri/tri_2.jpg";
 import tri_3 from "../../Images/Gallery/Elh/Tri/tri_3.jpg";
 import tri_4 from "../../Images/Gallery/Elh/Tri/tri_4.jpg";
+import tri_5 from "../../Images/Gallery/Elh/Tri/tri_5.webp";
 import vit from "../../Images/elh_vit.png";
 import vit_1 from "../../Images/Gallery/Elh/Vit/vit_1.jpg";
 import vit_2 from "../../Images/Gallery/Elh/Vit/vit_2.jpg";
 import vit_3 from "../../Images/Gallery/Elh/Vit/vit_3.jpg";
 import vit_4 from "../../Images/Gallery/Elh/Vit/vit_4.jpg";
+import vit_5 from "../../Images/Gallery/Elh/Vit/vit_5.webp";
 
 export const elh: Teams[] = [
   {
@@ -92,7 +106,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [pce_1, pce_2, pce_3, pce_4, pce_5]
+    photos: [pce_1, pce_2, pce_3, pce_4, pce_5],
+    arena_name: "Enteria arena",
+    arena_dedcription:
+      "Enteria arena (formerly Tipsport Arena or ČEZ Arena) is an indoor sporting arena in Pardubice, Czech Republic.",
+    arena_capacity: 10194,
+    arena_photo: pce_6
   },
   {
     id: 2,
@@ -118,7 +137,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [spa_1, spa_2, spa_3, spa_4]
+    photos: [spa_1, spa_2, spa_3, spa_4],
+    arena_name: "O2 Arena",
+    arena_dedcription:
+      "O2 Arena (stylised as O2 arena) is a multi-purpose arena, in Prague, Czech Republic.It is the third-largest ice hockey arena in Europe. It has hosted important sporting events such as three Ice Hockey World Championships (2004, 2015 and 2024), a handful of NHL games.",
+    arena_capacity: 17383,
+    arena_photo: spa_5
   },
   {
     id: 3,
@@ -144,7 +168,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [tri_1, tri_2, tri_3, tri_4]
+    photos: [tri_1, tri_2, tri_3, tri_4],
+    arena_name: "Werk Arena",
+    arena_dedcription:
+      "Werk Arena is an indoor sporting arena located in Třinec, Czech Republic.",
+    arena_capacity: 5400,
+    arena_photo: tri_5
   },
   {
     id: 4,
@@ -170,7 +199,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [kom_1, kom_2, kom_3]
+    photos: [kom_1, kom_2, kom_3],
+    arena_name: "Winning Group Arena",
+    arena_dedcription:
+      "Winning Group Arena, is an indoor arena in Brno, Czech Republic. Until 2011, the arena was named Hala Rondo. However, after Kajotbet started sponsoring the arena, it was renamed Kajot Arena. After the end of the contract, it was called 'Rondo Hall' again until a new sponsorship with DRFG was signed in 2015, renaming the arena to DRFG Arena.",
+    arena_capacity: 7700,
+    arena_photo: kom_4
   },
   {
     id: 5,
@@ -196,7 +230,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [ceb_1, ceb_2, ceb_3, ceb_4]
+    photos: [ceb_1, ceb_2, ceb_3, ceb_4],
+    arena_name: "Budvar Arena",
+    arena_dedcription:
+      "Budvar Arena is an indoor sporting arena located in České Budějovice, Czech Republic. The arena opened in October 1946 and has since undergone several renovations, being almost completely rebuilt in 2002.",
+    arena_capacity: 6421,
+    arena_photo: ceb_5
   },
   {
     id: 6,
@@ -222,7 +261,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [mhk_1, mhk_2, mhk_3, mhk_4]
+    photos: [mhk_1, mhk_2, mhk_3, mhk_4],
+    arena_name: "ČPP Arena",
+    arena_dedcription:
+      "ČPP Arena, is an arena in Hradec Králové, Czech Republic. It opened in 1957.",
+    arena_capacity: 6890,
+    arena_photo: mhk_5
   },
   {
     id: 7,
@@ -248,7 +292,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [lib_1, lib_2, lib_3]
+    photos: [lib_1, lib_2, lib_3],
+    arena_name: "Home Credit Arena",
+    arena_dedcription:
+      "The Home Credit Arena is an indoor sporting arena located in Liberec, Czech Republic. In its first five years of operation, the Arena welcomed 1.4 million visitors and hosted 467 events.",
+    arena_capacity: 7250,
+    arena_photo: lib_4
   },
   {
     id: 8,
@@ -274,7 +323,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [vit_1, vit_2, vit_3, vit_4]
+    photos: [vit_1, vit_2, vit_3, vit_4],
+    arena_name: "Ostravar Aréna",
+    arena_dedcription:
+      "OSTRAVAR Aréna is an indoor arena used mainly for ice hockey matches, in Vítkovice, Ostrava, Czech Republic. It opened in 1986, and underwent a € 23.3 million renovation between 2003 and 2004.",
+    arena_capacity: 10004,
+    arena_photo: vit_5
   },
   {
     id: 9,
@@ -300,7 +354,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [olo_1, olo_2, olo_3]
+    photos: [olo_1, olo_2, olo_3],
+    arena_name: "Zimní stadion Olomouc",
+    arena_dedcription:
+      "The Zimní stadion Olomouc is an indoor sporting arena located in Olomouc, Czech Republic. It was built in 1948 and roofed in 1967.",
+    arena_capacity: 5500,
+    arena_photo: olo_4
   },
   {
     id: 10,
@@ -326,7 +385,11 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [lit_1, lit_2, lit_3, lit_4]
+    photos: [lit_1, lit_2, lit_3, lit_4],
+    arena_name: "Zimní stadion Ivana Hlinky",
+    arena_dedcription: "",
+    arena_capacity: 6011,
+    arena_photo: lit_5
   },
   {
     id: 11,
@@ -352,7 +415,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [kva_1, kva_2, kva_3, kva_4, kva_5]
+    photos: [kva_1, kva_2, kva_3, kva_4, kva_5],
+    arena_name: "KV Arena",
+    arena_dedcription:
+      "KV Arena is an indoor sporting arena located in Karlovy Vary, Czech Republic, which opened on 19 June 2009. KV Arena comprises 4 interconnecting halls: Multi-purpose Hall (Main Hall), Training Hall, Ball Sports Hall and Swimming Pool.",
+    arena_capacity: 7500,
+    arena_photo: kva_6
   },
   {
     id: 12,
@@ -378,7 +446,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [plz_1, plz_2, plz_3]
+    photos: [plz_1, plz_2, plz_3],
+    arena_name: "Logspeed CZ Aréna",
+    arena_dedcription:
+      "LOGSPEED CZ Aréna is an indoor sporting arena located in Plzeň, Czech Republic. The ice rink was established in 1950 as an open-air stadium, and between 1966 and 1970 the area was developed to cover the playing surface.",
+    arena_capacity: 7356,
+    arena_photo: plz_4
   },
   {
     id: 13,
@@ -404,7 +477,12 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [mbl_1, mbl_2, mbl_3, mbl_4]
+    photos: [mbl_1, mbl_2, mbl_3, mbl_4],
+    arena_name: "Ško-Energo Aréna",
+    arena_dedcription:
+      "Ško-Energo Aréna is an indoor sporting arena located in Mladá Boleslav, Czech Republic.",
+    arena_capacity: 4200,
+    arena_photo: mbl_5
   },
   {
     id: 14,
@@ -430,6 +508,11 @@ export const elh: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [kla_1, kla_2, kla_3]
+    photos: [kla_1, kla_2, kla_3],
+    arena_name: "ČEZ Stadion",
+    arena_dedcription:
+      "ČEZ Stadion is an indoor sporting arena located in Kladno, Czech Republic. The arena, which is owned by the city of Kladno, opened in 1949, and the roof was completed in 1959.",
+    arena_capacity: 5200,
+    arena_photo: kla_4
   }
 ];
