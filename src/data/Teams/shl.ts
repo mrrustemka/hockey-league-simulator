b4443eccb9053301ec2c939c646a20167f6e5861
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2025-03-06 12:12:49 +0500

    implement-SHL-stadiums

--- src/data/Teams/shl.ts
+++ src/data/Teams/shl.ts
@@ -17,58 +17,72 @@ import aik_1 from "../../images/Gallery/Shl/Aik/aik_1.jpg";
 import aik_2 from "../../images/Gallery/Shl/Aik/aik_2.jpg";
 import aik_3 from "../../images/Gallery/Shl/Aik/aik_3.jpg";
 import aik_4 from "../../images/Gallery/Shl/Aik/aik_4.jpg";
+import aik_5 from "../../images/Gallery/Shl/Aik/aik_5.jpg";
 import fbk_1 from "../../images/Gallery/Shl/Fbk/fbk_1.jpg";
 import fbk_2 from "../../images/Gallery/Shl/Fbk/fbk_2.jpg";
 import fbk_3 from "../../images/Gallery/Shl/Fbk/fbk_3.jpg";
 import fbk_4 from "../../images/Gallery/Shl/Fbk/fbk_4.jpg";
+import fbk_5 from "../../images/Gallery/Shl/Fbk/fbk_5.webp";
 import fro_1 from "../../images/Gallery/Shl/Fro/fro_1.jpg";
 import fro_2 from "../../images/Gallery/Shl/Fro/fro_2.jpg";
 import fro_3 from "../../images/Gallery/Shl/Fro/fro_3.jpg";
 import fro_4 from "../../images/Gallery/Shl/Fro/fro_4.jpg";
+import fro_5 from "../../images/Gallery/Shl/Fro/fro_5.jpg";
 import hv71_1 from "../../images/Gallery/Shl/Hv71/hv71_1.jpg";
 import hv71_2 from "../../images/Gallery/Shl/Hv71/hv71_2.jpg";
 import hv71_3 from "../../images/Gallery/Shl/Hv71/hv71_3.jpg";
 import hv71_4 from "../../images/Gallery/Shl/Hv71/hv71_4.jpg";
+import hv71_5 from "../../images/Gallery/Shl/Hv71/hv71_5.webp";
 import iko_1 from "../../images/Gallery/Shl/Iko/iko_1.jpg";
 import iko_2 from "../../images/Gallery/Shl/Iko/iko_2.jpg";
 import iko_3 from "../../images/Gallery/Shl/Iko/iko_3.jpg";
 import iko_4 from "../../images/Gallery/Shl/Iko/iko_4.jpg";
+import iko_5 from "../../images/Gallery/Shl/Iko/iko_5.webp";
 import lak_1 from "../../images/Gallery/Shl/Lak/lak_1.jpg";
 import lak_2 from "../../images/Gallery/Shl/Lak/lak_2.jpg";
 import lak_3 from "../../images/Gallery/Shl/Lak/lak_3.jpg";
 import lak_4 from "../../images/Gallery/Shl/Lak/lak_4.jpg";
+import lak_5 from "../../images/Gallery/Shl/Lak/lak_5.jpg";
 import lhc_1 from "../../images/Gallery/Shl/Lhc/lhc_1.jpg";
 import lhc_2 from "../../images/Gallery/Shl/Lhc/lhc_2.jpg";
 import lhc_3 from "../../images/Gallery/Shl/Lhc/lhc_3.jpg";
 import lhc_4 from "../../images/Gallery/Shl/Lhc/lhc_4.jpg";
+import lhc_5 from "../../images/Gallery/Shl/Lhc/lhc_5.webp";
 import lhf_1 from "../../images/Gallery/Shl/Lhf/lhf_1.jpg";
 import lhf_2 from "../../images/Gallery/Shl/Lhf/lhf_2.jpg";
 import lhf_3 from "../../images/Gallery/Shl/Lhf/lhf_3.jpg";
 import lhf_4 from "../../images/Gallery/Shl/Lhf/lhf_4.jpg";
+import lhf_5 from "../../images/Gallery/Shl/Lhf/lhf_5.webp";
 import lif_1 from "../../images/Gallery/Shl/Lif/lif_1.jpg";
 import lif_2 from "../../images/Gallery/Shl/Lif/lif_2.jpg";
 import lif_3 from "../../images/Gallery/Shl/Lif/lif_3.jpg";
 import lif_4 from "../../images/Gallery/Shl/Lif/lif_4.jpg";
+import lif_5 from "../../images/Gallery/Shl/Lif/lif_5.webp";
 import mif_1 from "../../images/Gallery/Shl/Mif/mif_1.jpg";
 import mif_2 from "../../images/Gallery/Shl/Mif/mif_2.jpg";
 import mif_3 from "../../images/Gallery/Shl/Mif/mif_3.jpg";
 import mif_4 from "../../images/Gallery/Shl/Mif/mif_4.jpg";
+import mif_5 from "../../images/Gallery/Shl/Mif/mif_5.webp";
 import modo_1 from "../../images/Gallery/Shl/Modo/modo_1.jpg";
 import modo_2 from "../../images/Gallery/Shl/Modo/modo_2.jpg";
 import modo_3 from "../../images/Gallery/Shl/Modo/modo_3.jpg";
 import modo_4 from "../../images/Gallery/Shl/Modo/modo_4.jpg";
+import modo_5 from "../../images/Gallery/Shl/Modo/modo_5.webp";
 import ore_1 from "../../images/Gallery/Shl/Ore/ore_1.jpg";
 import ore_2 from "../../images/Gallery/Shl/Ore/ore_2.jpg";
 import ore_3 from "../../images/Gallery/Shl/Ore/ore_3.jpg";
 import ore_4 from "../../images/Gallery/Shl/Ore/ore_4.jpg";
+import ore_5 from "../../images/Gallery/Shl/Ore/ore_5.webp";
 import rbk_1 from "../../images/Gallery/Shl/Rbk/rbk_1.jpg";
 import rbk_2 from "../../images/Gallery/Shl/Rbk/rbk_2.jpg";
 import rbk_3 from "../../images/Gallery/Shl/Rbk/rbk_3.jpg";
 import rbk_4 from "../../images/Gallery/Shl/Rbk/rbk_4.jpg";
+import rbk_5 from "../../images/Gallery/Shl/Rbk/rbk_5.webp";
 import tik_1 from "../../images/Gallery/Shl/Tik/tik_1.jpg";
 import tik_2 from "../../images/Gallery/Shl/Tik/tik_2.jpg";
 import tik_3 from "../../images/Gallery/Shl/Tik/tik_3.jpg";
 import tik_4 from "../../images/Gallery/Shl/Tik/tik_4.jpg";
+import tik_5 from "../../images/Gallery/Shl/Tik/tik_5.webp";
 
 export const shl: Teams[] = [
   {
@@ -95,8 +109,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [lak_1, lak_2, lak_3, lak_4]
-  },
+    photos: [lak_1, lak_2, lak_3, lak_4],
+    arena_name: "Vida Arena",
+    arena_dedcription:
+      "Vida Arena (branded as VIDA Arena) is a multi-purpose indoor arena located in Växjö, Sweden. The arena opened in September 2011.",
+    arena_capacity: 5750,
+    arena_photo: lak_5
+},
   {
     id: 2,
     name: "AIK",
@@ -121,8 +140,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [aik_1, aik_2, aik_3, aik_4]
-  },
+    photos: [aik_1, aik_2, aik_3, aik_4],
+    arena_name: "Skellefteå Kraft Arena",
+    arena_dedcription:
+      "Skellefteå Kraft Arena is an indoor sporting arena located in Skellefteå, Sweden. It was built in 1966, and inaugurated on 6 January 1967. The arena is named after Skellefteå Kraft, a Swedish power company.",
+    arena_capacity: 5801,
+    arena_photo: aik_5
+},
   {
     id: 3,
     name: "Frolunda HC",
@@ -147,8 +171,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [fro_1, fro_2, fro_3, fro_4]
-  },
+    photos: [fro_1, fro_2, fro_3, fro_4],
+    arena_name: "Scandinavium",
+    arena_dedcription:
+      "Scandinavium is an indoor arena located in Gothenburg, Sweden. Construction on Scandinavium began in 1969 after decades of setbacks, and was inaugurated on 18 May 1971. Scandinavium has been selected as a championship arena at least fifty times, hosting events such as World Championships in handball and ice hockey, European championships, Davis Cup finals, and in 1985 the Eurovision Song Contest.",
+    arena_capacity: 12044,
+    arena_photo: fro_5
+},
   {
     id: 4,
     name: "Farjestad BK",
@@ -173,8 +202,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [fbk_1, fbk_2, fbk_3, fbk_4]
-  },
+    photos: [fbk_1, fbk_2, fbk_3, fbk_4],
+    arena_name: "Löfbergs Arena",
+    arena_dedcription:
+      "Löfbergs Arena is an arena located in Karlstad, Sweden, situated just above the river delta of Klarälven. It opened in 2001 following a complete overhaul and major expansion of the previously existing arena Färjestads Ishall. The name of the arena comes from the coffee roastery Löfbergs.",
+    arena_capacity: 8645,
+    arena_photo: fbk_5
+},
   {
     id: 5,
     name: "Timra IK",
@@ -199,8 +233,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [tik_1, tik_2, tik_3, tik_4]
-  },
+    photos: [tik_1, tik_2, tik_3, tik_4],
+    arena_name: "NHC Arena",
+    arena_dedcription:
+      "NHC Arena is an indoor sporting arena located in Timrå, Västernorrland County, Sweden. Construction began in 1964 and was inaugurated on 4 September 1966. Since then the venue has undergone several renovations and expansions.",
+    arena_capacity: 6000,
+    arena_photo: tik_5
+},
   {
     id: 6,
     name: "Lulea Hockey",
@@ -225,8 +264,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [lhf_1, lhf_2, lhf_3, lhf_4]
-  },
+    photos: [lhf_1, lhf_2, lhf_3, lhf_4],
+    arena_name: "Coop Norrbotten Arena",
+    arena_dedcription:
+      "Coop Norrbotten Arena is an indoor sporting arena located in Luleå, Sweden.",
+    arena_capacity: 6150,
+    arena_photo: lhf_5
+},
   {
     id: 7,
     name: "Leksands IF",
@@ -251,8 +295,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [lif_1, lif_2, lif_3, lif_4]
-  },
+    photos: [lif_1, lif_2, lif_3, lif_4],
+    arena_name: "Tegera Arena",
+    arena_dedcription:
+      "Tegera Arena (formerly Ejendals Arena) is an arena in Leksand, Sweden primarily. It was opened in October 2005.",
+    arena_capacity: 7650,
+    arena_photo: lif_5
+},
   {
     id: 8,
     name: "Linkoping HC",
@@ -277,8 +326,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [lhc_1, lhc_2, lhc_3, lhc_4]
-  },
+    photos: [lhc_1, lhc_2, lhc_3, lhc_4],
+    arena_name: "Saab Arena",
+    arena_dedcription:
+      "Saab Arena, formerly named Cloetta Center between 2004 and 2014, is an arena in Linköping, Sweden. It opened in September 2004.",
+    arena_capacity: 8500,
+    arena_photo: lhc_5
+},
   {
     id: 9,
     name: "Rogle BK",
@@ -303,8 +357,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [rbk_1, rbk_2, rbk_3, rbk_4]
-  },
+    photos: [rbk_1, rbk_2, rbk_3, rbk_4],
+    arena_name: "Catena Arena",
+    arena_dedcription:
+      "Catena Arena is an indoor arena in Ängelholm, Sweden. It was inaugurated on 20 September 2008.",
+    arena_capacity: 6310,
+    arena_photo: rbk_5
+},
   {
     id: 10,
     name: "Orebro Hockey",
@@ -329,8 +388,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [ore_1, ore_2, ore_3, ore_4]
-  },
+    photos: [ore_1, ore_2, ore_3, ore_4],
+    arena_name: "Behrn Arena",
+    arena_dedcription:
+      "Behrn Arena is an indoor arena located in Örebro, Sweden. The arena opened on 1 January 1965.On 27 March 1967, the Rolling Stones performed two shows at the arena, during their sixth European tour. Which was the only concert that was held at the venue.",
+    arena_capacity: 5500,
+    arena_photo: ore_5
+},
   {
     id: 11,
     name: "Modo Hockey",
@@ -355,8 +419,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [modo_1, modo_2, modo_3, modo_4]
-  },
+    photos: [modo_1, modo_2, modo_3, modo_4],
+    arena_name: "Hägglunds Arena",
+    arena_dedcription:
+      "Hägglunds Arena (formerly Swedbank Arena and Fjällräven Center) is an indoor sporting arena located in Örnsköldsvik, Sweden. The arena was opened on 26 August 2006.",
+    arena_capacity: 7265,
+    arena_photo: modo_5
+},
   {
     id: 12,
     name: "Malmö Redhawks",
@@ -381,8 +450,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [mif_1, mif_2, mif_3, mif_4]
-  },
+    photos: [mif_1, mif_2, mif_3, mif_4],
+    arena_name: "Malmö Arena",
+    arena_dedcription:
+      "Malmö Arena is a multi-purpose indoor arena in Malmö, Sweden. It is the largest arena in the SHL, and the second-largest indoor arena in Sweden.Owned and operated by Parkfast AB, the arena was designed by Mats Matson of MM Matsson Konsult AB, Hannu Helkiö of Pöyry Architects, and Gert Wingårdh of Wingårdh arkitektkontor.Naming rights for the venue are owned by Malmö Stad, in a ten-year contract, agreed in 2007. The venue hosted the 2014 World Junior Ice Hockey Championships from 26 December 2013 to 5 January 2014. Ground was broken on 10 January 2007, and the arena was inaugurated on 6 November 2008. The final construction cost for the arena was 750 million SEK. The arena is located about 80 m (260 ft) from Hyllie railway station, from where there are rail and bus connections to Malmö Central Station, Copenhagen Airport, and Copenhagen Central Station.",
+    arena_capacity: 12600,
+    arena_photo: mif_5
+},
   {
     id: 13,
     name: "HV71",
@@ -407,8 +481,13 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [hv71_1, hv71_2, hv71_3, hv71_4]
-  },
+    photos: [hv71_1, hv71_2, hv71_3, hv71_4],
+    arena_name: "Husqvarna Garden",
+    arena_dedcription:
+      "The Husqvarna Garden, formerly Kinnarps Arena, is an indoor arena.",
+    arena_capacity: 7000,
+    arena_photo: hv71_5
+},
   {
     id: 14,
     name: "IK Oskarshamn",
@@ -433,6 +512,11 @@ export const shl: Teams[] = [
     curStatusLength: 0,
     chartData: [],
     chartLabels: [],
-    photos: [iko_1, iko_2, iko_3, iko_4]
-  }
+    photos: [iko_1, iko_2, iko_3, iko_4],
+    arena_name: "Be-Ge Hockey Center",
+    arena_dedcription:
+      "Be-Ge Hockey Center, formerly Arena Oskarshamn, is the home ice of Swedish ice-hockey team IK Oskarshamn, located in the south-east of Sweden. The arena was completely rebuilt and renovated in 2004-2005. The arena, earlier known as Oskarshamns ishall, was inaugurated on 10 November 1974, with a game where Old Team Sweden defeated IK Oskarshamn, back then known as IK70, 6–2, in front of 2,600 spectators. The first international game inside the arena was played on 15 December 1974, a junior game where the Soviet Union defeated Sweden, 5–2.",
+    arena_capacity: 3275,
+    arena_photo: iko_5
+}
 ];
