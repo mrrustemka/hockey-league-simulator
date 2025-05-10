2c655e7e45015862a04d3ad444de21d8ca97e054
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2025-05-10 20:34:22 +0500

    implement-nhl-ehl-schedule

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -1,70 +1,100 @@
-import { v4 as uuidv4 } from "uuid";
-import { Schedule, Teams, TeamsList } from "./types";
+import { v4 as uuidv4 } from 'uuid';
+import { Schedule, Teams, TeamsList } from './types';
 
 let teamsList: TeamsList = [];
 export let scheduleList: Schedule[] = [];
 
+function getDoubleRoundSchedule(teams: Teams[]): Schedule[] {
+  const schedule: Schedule[] = [];
+
+  for (let i = 0; i < teams.length; i++) {
+    for (let j = 0; j < teams.length; j++) {
+      if (i !== j) {
+        schedule.push({
+          id: uuidv4(),
+          home: teams[i].abbreviation,
+          away: teams[j].abbreviation,
+        });
+      }
+    }
+  }
+
+  return shuffle(schedule);
+}
+
+function getOneRoundSchedule(teams: Teams[]): Schedule[] {
+  const schedule: Schedule[] = [];
+
+  for (let i = 0; i < teams.length; i++) {
+    for (let j = i + 1; j < teams.length; j++) {
+      schedule.push({
+        id: uuidv4(),
+        home: teams[i].abbreviation,
+        away: teams[j].abbreviation,
+      });
+    }
+  }
+
+  return shuffle(schedule);
+}
+
+function getNhlEhlSchedule(teams: Teams[]): Schedule[] {
+  const schedule: Schedule[] = [];
+
+  for (let i = 0; i < teams.length; i++) {
+    for (let j = 0; j < teams.length; j++) {
+      if (i !== j) {
+        schedule.push({
+          id: uuidv4(),
+          home: teams[i].abbreviation,
+          away: teams[j].abbreviation,
+        });
+      }
+    }
+  }
+
+  for (let i = 0; i < teams.length; i++) {
+    for (let j = i + 1; j < teams.length; j++) {
+      if (teams[i].conferenceId === teams[j].conferenceId) {
+        schedule.push({
+          id: uuidv4(),
+          home: teams[i].abbreviation,
+          away: teams[j].abbreviation,
+        });
+      }
+    }
+  }
+
+  return shuffle(schedule);
+}
+
+function shuffle(schedule: Schedule[]): Schedule[] {
+  for (let k = schedule.length - 1; k > 0; k--) {
+    const randomIndex = Math.floor(Math.random() * (k + 1));
+    [schedule[k], schedule[randomIndex]] = [schedule[randomIndex], schedule[k]];
+  }
+
+  return schedule;
+}
+
 export function schedule(teams: Teams[], id: string) {
-  scheduleList =
-    id === "1" || id === "2"
-      ? getOneRoundSchedule(teams)
-      : getDoubleRoundSchedule(teams);
+  scheduleList = [];
+
+  if (id === '1' || id === '2') {
+    scheduleList = getOneRoundSchedule(teams);
+  } else if (id === '3' || id === '4') {
+    scheduleList = getNhlEhlSchedule(teams);
+  } else {
+    scheduleList = getDoubleRoundSchedule(teams);
+  }
+
   teamsList = [];
+
   teams.forEach((team) => {
     if (team) return teamsList.push(team.abbreviation);
   });
 
-  localStorage.setItem("teamsList", JSON.stringify(teams));
-  localStorage.setItem("scheduleList", JSON.stringify(scheduleList));
-  localStorage.setItem("gameIndex", JSON.stringify(0));
-
-  function getDoubleRoundSchedule(teams: Teams[]): Schedule[] {
-    const schedule: Schedule[] = [];
-
-    for (let i = 0; i < teams.length; i++) {
-      for (let j = 0; j < teams.length; j++) {
-        if (i !== j) {
-          schedule.push({
-            id: uuidv4(),
-            home: teams[i].abbreviation,
-            away: teams[j].abbreviation
-          });
-        }
-      }
-    }
-
-    for (let k = schedule.length - 1; k > 0; k--) {
-      const randomIndex = Math.floor(Math.random() * (k + 1));
-      [schedule[k], schedule[randomIndex]] = [
-        schedule[randomIndex],
-        schedule[k]
-      ];
-    }
-
-    return schedule;
-  }
-
-  function getOneRoundSchedule(teams: Teams[]): Schedule[] {
-    const schedule: Schedule[] = [];
-
-    for (let i = 0; i < teams.length; i++) {
-      for (let j = i + 1; j < teams.length; j++) {
-        schedule.push({
-          id: uuidv4(),
-          home: teams[i].abbreviation,
-          away: teams[j].abbreviation
-        });
-      }
-    }
-
-    for (let k = schedule.length - 1; k > 0; k--) {
-      const randomIndex = Math.floor(Math.random() * (k + 1));
-      [schedule[k], schedule[randomIndex]] = [
-        schedule[randomIndex],
-        schedule[k]
-      ];
-    }
-
-    return schedule;
-  }
+  localStorage.setItem('teamsList', JSON.stringify(teams));
+  localStorage.setItem('scheduleList', JSON.stringify(scheduleList));
+  localStorage.setItem('gameIndex', JSON.stringify(0));
 }
