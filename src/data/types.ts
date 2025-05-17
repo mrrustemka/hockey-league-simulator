a40459e029db9ab5764023f31bd44c6ab1c7ccab
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2025-05-17 14:51:25 +0500

    implement-readonly-property-to-types

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,54 +1,54 @@
 export type Teams = {
-  abbreviation: string;
-  city: string;
-  country: string;
+  readonly abbreviation: string;
+  readonly city: string;
+  readonly country: string;
   game_counter: number;
   goals_against: number;
   goals_diff: number;
   goals_for: number;
   readonly id: string;
-  logo: string;
+  readonly logo: string;
   loses: number;
   loses_ot: number;
-  name: string;
+  readonly name: string;
   points: number;
   rating: number;
   wins: number;
   play_off_rank: number;
   play_off_round_wins: number;
-  color: string;
-  flag?: string;
+  readonly color: string;
+  readonly flag?: string;
   is_playoff: boolean;
   status: string;
   cur_status_length: number;
   chart_data: number[];
   chart_labels: string[];
-  photos: string[];
-  arena_name?: string;
-  arena_description?: string;
-  arena_capacity?: number;
-  arena_photo?: string;
-  facebook: string;
-  youtube: string;
-  instagram: string;
-  x: string;
-  threads: string;
-  tiktok: string;
-  snapchat: string;
-  linkedin: string;
-  website: string;
-  twitch: string;
+  readonly photos: string[];
+  readonly arena_name?: string;
+  readonly arena_description?: string;
+  readonly arena_capacity?: number;
+  readonly arena_photo?: string;
+  readonly facebook: string;
+  readonly youtube: string;
+  readonly instagram: string;
+  readonly x: string;
+  readonly threads: string;
+  readonly tiktok: string;
+  readonly snapchat: string;
+  readonly linkedin: string;
+  readonly website: string;
+  readonly twitch: string;
   points_percentage: number;
-  conference_id?: string;
-  division_id?: string;
+  readonly conference_id?: string;
+  readonly division_id?: string;
 };
 
 export type TeamsList = string[];
 
 export type Schedule = {
   readonly id: string;
-  away: string;
-  home: string;
+  readonly away: string;
+  readonly home: string;
 };
 
 export type GameResult = {
@@ -73,23 +73,23 @@ export type PlayOffGameResult = {
 
 export type League = {
   readonly id: string;
-  name: string;
-  description: string;
-  image: string;
+  readonly name: string;
+  readonly description: string;
+  readonly image: string;
   teams: Teams[];
   teams_count: number;
-  cup: string;
-  conferences?: Conference[];
-  divisions?: Division[];
+  readonly cup: string;
+  readonly conferences?: Conference[];
+  readonly divisions?: Division[];
 };
 
 export type Conference = {
-  conference_id: string;
-  name: string;
+  readonly conference_id: string;
+  readonly name: string;
 };
 
 export type Division = {
-  name: string;
-  division_id: string;
-  conference_id: string;
+  readonly name: string;
+  readonly division_id: string;
+  readonly conference_id: string;
 };
